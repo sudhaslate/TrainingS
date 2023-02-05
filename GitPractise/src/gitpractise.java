0277dff536f6498f8e5b0b99759c4639816995ff
ptummati <sudhaslate@gmail.com>
2023-02-05 23:49:23 +0000

    My Java Methods

--- GitPractise/src/gitpractise.java
+++ GitPractise/src/gitpractise.java
@@ -2,11 +2,6 @@ public class gitpractise {
     public static void main(String[] args) {
         System.out.println("My World of Learning");
 
-        int a=33; int b= 34;
-        System.out.println("Product of a&b =" + (a*b));
-
-        int m=33; int v= 34;
-        System.out.println("Product of v,m =" + (v*m));
 
     }
 }
