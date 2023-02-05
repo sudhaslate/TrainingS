9d6135b813da4aecddb663e03bf39a6238d02e5d
ptummati <sudhaslate@gmail.com>
2023-02-05 08:16:47 +0000

    My first Java commit

---
+++ GitPractise/src/gitpractise.java
@@ -0,0 +1,8 @@
+public class gitpractise {
+    public static void main(String[] args) {
+        System.out.println("My World of Learning");
+
+        int a=33; int b= 34;
+        System.out.println("Product of a&b =" + (a*b));
+    }
+}
