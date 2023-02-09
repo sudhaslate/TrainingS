44a94864c5a3e08a80a57e56c42702a1d743033e
ptummati <sudhaslate@gmail.com>
2023-02-09 22:02:57 +0000

    MOL

---
+++ JInheritance/src/keywordthis/MethodOverLoading.java
@@ -0,0 +1,23 @@
+package keywordthis;
+
+public class MethodOverLoading {
+  void add(int a, int b){               //1 method with parameters as int, int
+      System.out.println(a+b);
+  }
+    void add(int a, double b){         //2  method with parameters as int, double
+        System.out.println(a+b);
+    }
+    void add(double a, double b){         //3 method with parameters as double, double
+        System.out.println(a+b);
+    }
+    void add(int a, int b,int c){        //4 method with parameters as int, int, int
+        System.out.println(a+b+c);
+    }
+    public static void main(String[] args) {
+        MethodOverLoading ml = new MethodOverLoading();     //object
+        ml.add(10,23);
+        ml.add(23,3.45);
+        ml.add(8.9, 9.99);
+        ml.add(2,34,45);
+    }
+}
