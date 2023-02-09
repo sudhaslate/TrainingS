7e00616df6b2d88d61d51fff2f9fa302b1c72c70
ptummati <sudhaslate@gmail.com>
2023-02-09 23:28:07 +0000

    Final Keyword..

---
+++ JInheritance/src/superKeyword/FinalKeyword.java
@@ -0,0 +1,14 @@
+package superKeyword;
+
+// 1.Variable: You cannot change the value of the variable
+// 2.Method: method cannot be oerrid in the child classes
+// 3.Class:
+
+public class FinalKeyword {
+    final int speed=80;     //final variable,so we cannot change this variable
+    public static void main(String[] args) {
+        FinalKeyword fk = new FinalKeyword();
+        fk.speed=80;     //compile time error
+        System.out.println(fk.speed);
+    }
+}
